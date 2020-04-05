e659916a89a83448b5fc9111f1fa6527668d97dd
David Banks <amoebae@gmail.com>
2020-04-05 16:49:34 +0100

    demonstrate using a test double

--- test/test_gear.py
+++ test/test_gear.py
@@ -1,7 +1,12 @@
 from pytest import approx
 from main import Gear, Wheel
 
-def test_calculates_gear_inches():
+class DiameterDouble:
+    def diameter(self):
+        return 10
+
+# Use the real Wheel class to provide the hidden diameter calculation.
+def test_calculates_gear_inches_with_real_dependency():
     chainring = 52
     cog = 11
     rim = 26
@@ -11,3 +16,13 @@ def test_calculates_gear_inches():
     gear = Gear(chainring, cog, wheel)
     
     assert gear.gear_inches() == approx(137.1, rel=1e-4)
+
+
+# Use a test double.
+def test_calculates_gear_inches_with_test_double():
+    chainring = 52
+    cog = 11
+
+    gear = Gear(chainring, cog, DiameterDouble())
+    
+    assert gear.gear_inches() == approx(47.27, rel=1e-4)
