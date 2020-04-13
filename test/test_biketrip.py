6feaf7413c00c1a2924e9451002e7edf62ebed8c
David Banks <amoebae@gmail.com>
2020-04-13 12:35:46 +0100

    test that Trip sends outgoing message correctly

--- test/test_biketrip.py
+++ test/test_biketrip.py
@@ -1,5 +1,6 @@
-from metz_pood.biketrip import Mechanic, TripCoordinator, Driver
+from metz_pood.biketrip import Mechanic, TripCoordinator, Driver, Trip
 from metz_pood.test_utility import responds_to
+from unittest.mock import Mock
 
 def test_mechanic_implements_the_preparer_interface():
     assert responds_to(Mechanic(), 'prepare_trip')
@@ -11,5 +12,8 @@ def test_tripcoordinator_implements_the_preparer_interface():
     assert responds_to(Driver(), 'prepare_trip')
 
     
-
-
+def test_requests_trip_preparation():
+    mock_preparer = Mock()
+    trip = Trip([], [], [])
+    trip.prepare([mock_preparer])
+    mock_preparer.prepare_trip.assert_called_with(trip)
