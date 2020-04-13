6e41ea38c5d74e0b91c8885c67df4f4a66ceeae7
David Banks <amoebae@gmail.com>
2020-04-13 12:15:49 +0100

    adding bike trip

---
+++ metz_pood/biketrip.py
@@ -0,0 +1,18 @@
+class Mechanic:
+    def prepare_bicycles(self, bicycles):
+        for bicycle in bicycles:
+            self.prepare_bicycle(bicycle)
+    
+    def prepare_bicycle(self, bicycle):
+        pass
+
+class TripCoordinator:
+    def buy_food(self, customers):
+        pass
+    
+class Driver:
+    def gas_up(self, vehicle):
+        pass
+
+    def fill_water_tank(self, vehicle):
+        pass
---
+++ test/test_biketrip.py
@@ -0,0 +1,4 @@
+import metz_pood.biketrip
+
+def test_sanity():
+    assert True
